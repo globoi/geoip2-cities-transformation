dc1c969cfefa700d165f57cd26b4d0b0d294371b
fferreir <felipe.ferreira@g.globo>
2023-10-24 17:40:54 -0300

    tests: adds conformity tests to network field

--- definitions/raw/conformity.js
+++ definitions/raw/conformity.js
@@ -1,8 +1,16 @@
-tables = ["city_blocks_ipv4", "city_blocks_ipv6"]
+rules = [
+    {
+        table_name: "city_blocks_ipv4",
+        regex: "r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/([0-9]|[1-2][0-9]|3[0-2])$'"
+    },
+    {
+        table_name: "city_blocks_ipv6",
+        regex: "r'^([0-9a-fA-F]{0,4}:){2,7}[0-9a-fA-F]{0,4}/([0-9]|[1-9][0-9]|1[0-1][0-9]|12[0-8])$'"
+    }]
 
-tables.map((table) => assert(`conformity_${table}`).query((ctx) => `
+rules.map((rule) => assert(`conformity_${rule.table_name}`).query((ctx) => `
     SELECT
-        NOT REGEXP_CONTAINS(network, r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/([0-9]|[1-2][0-9]|3[0-2])$')
+        NOT REGEXP_CONTAINS(network, ${rule.regex})
     FROM
         ${ctx.ref(table)}
 `))
