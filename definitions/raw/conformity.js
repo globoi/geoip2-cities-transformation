f979502b0eea4fcc4e7d4b2ed8b600f0b053aadd
fferreir <felipe.ferreira@g.globo>
2023-10-24 17:34:14 -0300

    tests: adds conformity tests to network field

---
+++ definitions/raw/conformity.js
@@ -0,0 +1,8 @@
+tables = ["city_blocks_ipv4", "city_blocks_ipv6"]
+
+tables.map((table) => assert(`conformity_${table}`).query((ctx) => `
+    SELECT
+        NOT REGEXP_CONTAINS(network, r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/([0-9]|[1-2][0-9]|3[0-2])$')
+    FROM
+        ${ctx.ref("city_blocks_ipv4")}
+`))
