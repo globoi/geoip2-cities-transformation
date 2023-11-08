2d303dfa7f74bc53f59ebd93aa4e41e00da0470d
fferreir <felipe.ferreira@g.globo>
2023-11-08 12:57:09 -0300

    docs: adds missing descriptions

--- definitions/raw/precision_coordinates.js
+++ definitions/raw/precision_coordinates.js
@@ -1,26 +1,28 @@
-tables = ["city_blocks_ipv4", "city_blocks_ipv6"]
-fields = [
-    {
-        name: "latitude",
-        minValue: -90,
-        maxValue: 90
-    },
-    {
-        name: "longitude",
-        minValue: -180,
-        maxValue: 180
-    }
+const tables = ['city_blocks_ipv4', 'city_blocks_ipv6']
+const fields = [
+  {
+    name: 'latitude',
+    minValue: -90,
+    maxValue: 90,
+  },
+  {
+    name: 'longitude',
+    minValue: -180,
+    maxValue: 180,
+  },
 ]
 
 tables.map((table) => {
-    fields.map((field) => {
-        assert(`precision_${table}_${field.name}`).query((ctx) => `
-            SELECT
-                ${field.name} AS limite_${field.name}
-            FROM
-                ${ctx.ref("raw", table)}
-            WHERE
-                ${field.name} <= ${field.minValue} AND ${field.name} >= ${field.maxValue}
-        `)
+  fields.map((field) => {
+    assert(`precision_${table}_${field.name}`).query((ctx) => `
+        SELECT
+            ${field.name} AS limite_${field.name}
+        FROM
+            ${ctx.ref('raw', table)}
+        WHERE
+            ${field.name} <= ${field.minValue} AND ${field.name} >= ${field.maxValue}
+    `).config({
+      description: 'Verifica se latitude e langitude tem valores dentro do limite esperado',
     })
-});
+  })
+})
